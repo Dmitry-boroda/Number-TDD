a30298a3d03ca162313646c165e4754a7e079641
Dmitry-boroda <dima-krvchk@yandex.ru>
2022-10-26 23:28:17 +0300

    Create Communication,Mapper and implement NumberResult, UiState

--- app/src/main/java/com/example/number/numbers/presentation/NumbersCommunications.kt
+++ app/src/main/java/com/example/number/numbers/presentation/NumbersCommunications.kt
@@ -6,4 +6,22 @@ interface NumbersCommunications {
     fun showState(state: UiState)
 
     fun showList(list: List<NumberUi>)
+
+    class Base(
+
+    ) {
+
+    }
+}
+
+interface ProgressCommunication : Communication.Mutable<Boolean> {
+    class Base() : Communication.Post<Boolean>(), ProgressCommunication
+}
+
+interface NumbersState : Communication.Mutable<UiState> {
+    class Base() : Communication.Post<UiState>(), NumbersState
+}
+
+interface NumbersList : Communication.Mutable<List<NumberUi>> {
+    class Base() : Communication.Post<List<NumberUi>>(), NumbersList
 }
