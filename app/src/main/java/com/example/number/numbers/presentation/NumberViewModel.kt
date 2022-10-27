e26733e196f0ac6e5fb3ef2be80979229268aa3a
admin <dima-krvchk@yandex.ru>
2022-10-27 17:05:24 +0300

    Create Observer

--- app/src/main/java/com/example/number/numbers/presentation/NumberViewModel.kt
+++ app/src/main/java/com/example/number/numbers/presentation/NumberViewModel.kt
@@ -1,9 +1,19 @@
 package com.example.number.numbers.presentation
 
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.Observer
 import com.example.number.numbers.domain.NumbersInteractor
 
 class NumberViewModel(
     private val communication: NumbersCommunications,
     private val interactor: NumbersInteractor
-    ) {
+    ):ObserveNumbers {
+    override fun observeProgress(owner: LifecycleOwner, observer: Observer<Boolean>) =
+        communication.observeProgress(owner, observer)
+
+    override fun observeState(owner: LifecycleOwner, observer: Observer<UiState>) =
+        communication.observeState(owner, observer)
+
+    override fun observeList(owner: LifecycleOwner, observer: Observer<List<NumberUi>>) =
+        communication.observeList(owner, observer)
 }
