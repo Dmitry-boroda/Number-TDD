f388e730b5c6de568ab995ad70e5a7681fdffe78
Dmitry-boroda <dima-krvchk@yandex.ru>
2022-11-30 22:39:46 +0300

    cloud data source

---
+++ app/src/main/java/com/example/number/main/NumbersApp.kt
@@ -0,0 +1,17 @@
+package com.example.number.main
+
+import android.app.Application
+import com.example.number.BuildConfig
+import com.example.number.numbers.data.CloudModule
+
+class NumbersApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        // todo move out of her
+        val cloudDataSource = if (BuildConfig.DEBUG)
+            CloudModule.Debug()
+        else
+            CloudModule.Release()
+    }
+}
---
+++ app/src/main/java/com/example/number/numbers/data/CloudModule.kt
@@ -0,0 +1,41 @@
+package com.example.number.numbers.data
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
+interface CloudModule {
+
+    fun <T> service(clasz: Class<T>): T
+
+    abstract class Abstract : CloudModule {
+
+        protected abstract val level: HttpLoggingInterceptor.Level
+        protected abstract val baseUrl: String
+        override fun <T> service(clasz: Class<T>): T {
+            val interceptor = HttpLoggingInterceptor().apply {
+                setLevel(level)
+            }
+            val client = OkHttpClient.Builder()
+                .addInterceptor(interceptor)
+                .build()
+            val retrofit = Retrofit.Builder()
+                .client(client)
+                .baseUrl(baseUrl)
+                .addConverterFactory(ScalarsConverterFactory.create())
+                .build()
+            return retrofit.create(clasz)
+        }
+    }
+
+    class Release : Abstract() {
+        override val level = HttpLoggingInterceptor.Level.NONE
+        override val baseUrl = "http://numbersapi.com/"
+    }
+
+    class Debug : Abstract() {
+        override val level = HttpLoggingInterceptor.Level.BODY
+        override val baseUrl = "http://numbersapi.com/"
+    }
+}
